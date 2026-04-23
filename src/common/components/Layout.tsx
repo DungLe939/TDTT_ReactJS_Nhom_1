826b67eb4f30a8a56f4ad2aa45017bbab6717a20
BaThi-hcmus <huynhbathi2006@gmail.com>
2026-04-23 09:09:45 +0700

    [UPDATE] xu li conflic khi merge voi develop

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -178,7 +178,7 @@ export const Layout = () => {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 relative z-10">
+      <main className="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 relative">
         <Outlet />
       </main>
 
