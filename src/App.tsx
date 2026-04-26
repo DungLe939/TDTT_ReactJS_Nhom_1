98b8b9907934d468877919aae0bc32c989ac9df5
DungLe939 <suseer1006@gmail.com>
2026-04-26 08:26:09 +0700

    [UPDATE] Layout website

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { router } from './routes';
 import { AuthProvider } from './modules/auth/context/AuthContext';
 import { Toaster } from 'sonner';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './common/components/ThemeProvider';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-right" richColors />
-      </AuthProvider>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="light" enableSystem={false} attribute="class">
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" richColors />
+        </AuthProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
